2cfa5edfd52fff751f34f0445bc8e8d4df3a95fb
Grady McPeak <gradymcpeak@gmail.com>
2025-11-11 12:09:56 -0500

    Add supporting utils

---
+++ utils/box_client_jwt.py
@@ -0,0 +1,63 @@
+# Duplicated from https://github.com/box-community/box-python-gen-workshop
+"""
+Handles the box client object creation
+orchestrates the authentication process
+"""
+
+import os
+
+import dotenv
+from box_sdk_gen import (
+    BoxClient,
+    BoxJWTAuth,
+    FileWithInMemoryCacheTokenStorage,
+    JWTConfig,
+)
+
+ENV_JET = ".jwt.env"
+
+
+class ConfigJWT:
+    """application configurations"""
+
+    def __init__(self) -> None:
+        dotenv.load_dotenv(ENV_JET)
+
+        # JWT configurations
+        self.jwt_config_path = os.getenv("JWT_CONFIG_PATH")
+        self.jwt_user_id = os.getenv("JWT_USER_ID")
+        self.enterprise_id = os.getenv("ENTERPRISE_ID")
+
+        self.cache_file = os.getenv("CACHE_FILE", ".jwt.tk")
+
+    def __repr__(self) -> str:
+        return f"ConfigJWT({self.__dict__})"
+
+
+def get_jwt_enterprise_client(config: ConfigJWT) -> BoxClient:
+    """Returns a box sdk Client object"""
+
+    jwt = JWTConfig.from_config_file(
+        config_file_path=config.jwt_config_path,
+        token_storage=FileWithInMemoryCacheTokenStorage(".ent" + config.cache_file),
+    )
+    auth = BoxJWTAuth(jwt)
+
+    client = BoxClient(auth)
+
+    return client
+
+
+def get_jwt_user_client(config: ConfigJWT, user_id: str) -> BoxClient:
+    """Returns a box sdk Client object"""
+
+    jwt = JWTConfig.from_config_file(
+        config_file_path=config.jwt_config_path,
+        token_storage=FileWithInMemoryCacheTokenStorage(".user" + config.cache_file),
+    )
+    auth = BoxJWTAuth(jwt)
+    auth = auth.with_user_subject(user_id)
+
+    client = BoxClient(auth)
+
+    return client
---
+++ utils/box_utils.py
@@ -0,0 +1,82 @@
+# Duplicated from https://github.com/box-community/box-python-gen-workshop
+
+import logging
+import os
+import pathlib
+
+from box_sdk_gen import BoxAPIError
+from box_sdk_gen.client import BoxClient as Client
+from box_sdk_gen.managers.folders import CreateFolderParent
+from box_sdk_gen.managers.uploads import (
+    PreflightFileUploadCheckParent,
+    UploadFileAttributes,
+    UploadFileAttributesParentField,
+)
+from box_sdk_gen.schemas import File, Files, Folder
+
+logging.getLogger(__name__)
+
+
+def create_box_folder(client: Client, folder_name: str, parent_folder: Folder) -> Folder:
+    """create a folder in box"""
+
+    try:
+        folder = client.folders.create_folder(folder_name, CreateFolderParent(id=parent_folder.id))
+    except BoxAPIError as err:
+        if err.response_info.body.get("code", None) == "item_name_in_use":
+            folder_id = err.response_info.body["context_info"]["conflicts"][0]["id"]
+            folder = client.folders.get_folder_by_id(folder_id)
+        else:
+            raise err
+
+    logging.info("Folder %s with id: %s", folder.name, folder.id)
+    return folder
+
+
+def folder_upload(
+    client: Client,
+    box_base_folder: Folder,
+    local_folder_path: str,
+) -> Folder:
+    """upload a folder to box"""
+
+    local_folder = pathlib.Path(local_folder_path)
+
+    for item in local_folder.iterdir():
+        if item.is_dir():
+            new_box_folder = create_box_folder(client, item.name, box_base_folder)
+            logging.info(" Folder %s", item.name)
+            folder_upload(client, new_box_folder, str(item))
+        else:
+            file = file_upload(client, str(item), box_base_folder)
+            logging.info(" \tUploaded %s (%s) %s bytes", file.name, file.id, file.size)
+
+    return box_base_folder
+
+
+def file_upload(client: Client, file_path: str, folder: Folder) -> File:
+    """upload a file to box"""
+
+    file_size = os.path.getsize(file_path)
+    file_name = os.path.basename(file_path)
+    file_id = None
+    try:
+        pre_flight_arg = PreflightFileUploadCheckParent(id=folder.id)
+        client.uploads.preflight_file_upload_check(name=file_name, size=file_size, parent=pre_flight_arg)
+    except BoxAPIError as err:
+        if err.response_info.body.get("code", None) == "item_name_in_use":
+            file_id = err.response_info.body["context_info"]["conflicts"]["id"]
+        else:
+            raise err
+
+    upload_arg = UploadFileAttributes(file_name, UploadFileAttributesParentField(folder.id))
+    if file_id is None:
+        # upload new file
+        files: Files = client.uploads.upload_file(upload_arg, file=open(file_path, "rb"))
+        file = files.entries[0]
+    else:
+        # upload new version
+        files: Files = client.uploads.upload_file_version(file_id, upload_arg, file=open(file_path, "rb"))
+        file = files.entries[0]
+
+    return file
